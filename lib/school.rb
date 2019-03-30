2344196fd8b36ff14c4af25d5f084f431b3c1362
Alex Merecka <merecka@gmail.com>
2019-03-30 02:01:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade_num)
+    @roster[grade_num]
+  end
+
+  def sort
+    @roster.collect do |grade, name|
+      name.sort!
+    end
+    @roster
+  end
+
+end
